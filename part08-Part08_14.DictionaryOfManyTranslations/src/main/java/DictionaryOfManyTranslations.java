459abb58744603be4806d1467207e006479478ab
Keji8080 <kevinjimenezuk@gmail.com>
2024-07-16 12:35:55 +0200

    Finished exercise with notes

--- part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
+++ part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -6,23 +6,23 @@ import java.util.HashMap;
 
 public class DictionaryOfManyTranslations {
     
-    private HashMap<String, ArrayList<String>> dictionary;
+    private HashMap<String, ArrayList<String>> dictionary; // Creamos un hashmap de tipo string (para la palabra) y ArrayList (para todas las traducciones)
 
     public DictionaryOfManyTranslations() {
-        this.dictionary = new HashMap<>();
+        this.dictionary = new HashMap<>(); // Declaramos la creacion de un hashmap cada vez que usemos el constructor
     }
     
-    public void add(String word, String translation) {
-        this.dictionary.putIfAbsent(word, new ArrayList<>());
+    public void add(String word, String translation) { // Metodo para agregar la palabra y sus traducciones
+        this.dictionary.putIfAbsent(word, new ArrayList<>()); // putIfAbsent asegura que se inicia la lista si no existe
         
-        this.dictionary.get(word).add(translation);        
+        this.dictionary.get(word).add(translation);  // Agregamos al key word, las traducciones a nuestra lista     
     }
     
     public ArrayList<String> translate(String word) {
-        return this.dictionary.getOrDefault(word, new ArrayList<>());
+        return this.dictionary.getOrDefault(word, new ArrayList<>()); //usamos getOrDefault si la clave no esta en el hashmap, este metodo devolvera un arrayList vacio por defecto
     }
     
     public void remove(String word) {
-            dictionary.remove(word);
+            dictionary.remove(word); //removemos el key y el valor asociado
     }  
 }
